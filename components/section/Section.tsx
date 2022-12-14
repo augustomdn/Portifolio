dbb63fd8ca1bdaa59be3b4ff7e4bb9a4e59138da
Augusto Meireles do Nascimento <augustomeirelesn@hotmail.com>
2022-12-14 16:16:15 -0300

    Commit do projeto PokedexApi

--- components/section/Section.tsx
+++ components/section/Section.tsx
@@ -44,7 +44,7 @@ export const Section = () => {
                     </span>
                     <div className={styles.figureDiv}>
                         <h1 className={styles.figureTitle}>Components</h1>
-                        <i className={styles.figureText}>Criação de componentes utilizando o Storybook para documentá-los e utilizá-los de maneira isolada </i>
+                        <i className={styles.figureText}>Criação de componentes utilizando o Storybook para documentá-los e utilizá-los de maneira isolada</i>
 
                         <div className={styles.figureButtonDiv}>
                             <a href="https://github.com/augustomdn/Components">
@@ -76,6 +76,29 @@ export const Section = () => {
                         </div>
                     </div>
                 </figure>
+                <figure className={styles.figureCard}>
+                    <span className={styles.figureImage}>
+                        <Image
+                            className={styles.figureImageRound}
+                            width={200}
+                            height={124}
+                            layout={'responsive'}
+                            src={'/pokemons.png'}
+                            alt="umv-image" />
+                    </span>
+                    <div className={styles.figureDiv}>
+                        <h1 className={styles.figureTitle}>Pokedex API</h1>
+                        <i className={styles.figureText}>Pokedex criada utilizando uma interface API RESTful para a obtenção de dados</i>
+                        <div className={styles.figureButtonDiv}>
+                            <a href="https://github.com/augustomdn/pokedex-javascript">
+                                <button className={styles.figureButton}>Code</button>
+                            </a>
+                            <a href="https://pokedex-javascript-api.vercel.app/">
+                                <button className={styles.figureButton}>Website</button>
+                            </a>
+                        </div>
+                    </div>
+                </figure>   
             </div>
         </section >
     )
